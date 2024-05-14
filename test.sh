c9f735e2f12e866b6e577a760a0de43b5fe29385
alexpinch <alexpinch@hotmail.com>
2024-05-14 16:15:29 -0700

    Giving up user input is hard

--- test.sh
+++ test.sh
@@ -5,26 +5,23 @@ random_line=$((RANDOM % (line_count - 1) + 2))
 answer=$(awk -v line="$random_line" 'NR == line {print $0}' birds.csv | cut -d ',' -f1)
 echo "Selected line: $random_line"
 echo "Answer: $answer"
-read -p "Guess a bird: " guess
+read -p "Guess the mystery bird: " guess
 if [ "$guess" = "$answer" ]; then
-    echo "Correct"
+    echo "Correct!"
 else
-    # Look for the guess in birds.csv
     guess_found=$(awk -v guess="$guess" '$0 ~ guess {print NR}' birds.csv)
-
     if [ -n "$guess_found" ]; then
-        echo "Guess found in birds.csv at line $guess_found"
-
-        # Check if any of the fields on $random_line match the lines on $guess_found
-        fields_random_line=$(awk -v line="$random_line" 'NR == line' birds.csv)
-        fields_guess_found=$(awk -v line="$guess_found" 'NR == line' birds.csv)
-
+        fields_random_line=$(awk -v line="$random_line" 'NR == line {print $2,$3,$4}' birds.csv)
+        fields_guess_found=$(awk -v line="$guess_found" 'NR == line {print $2,$3,$4}' birds.csv)
+        echo "fields_random_line: $fields_random_line"
+        echo "fields_guess_found: $fields_guess_found"
         if [ "$fields_random_line" = "$fields_guess_found" ]; then
             echo "Fields on the random line match the fields on the guess found"
+            
         else
-            echo "Fields on the random line do not match the fields on the guess found"
+            echo "Nothing in common with the mystery bird."
         fi
     else
-        echo "Guess not found in birds.csv"
+        echo "Unrecognized bird"
     fi
 fi
